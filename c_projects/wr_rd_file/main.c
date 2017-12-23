9e85e7696b6bb3f04260c4711440515eaba12384
gazzda <gazzda@gmail.com>
2017-12-23 22:14:49 +0100

    added main.c for wr_rd_file test

---
+++ c_projects/wr_rd_file/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+   const char *text = "Test text";        // Some text
+
+   FILE *fp;                              
+
+   fp = fopen ("File.txt", "w");          // Create and open file with name "File.txt"
+   
+   if (fp == NULL)                        // Error if file not created
+   {
+      printf("Error opening file!\n");
+      return 1;
+   }
+   
+   fprintf (fp, "Text: %s\n", text);      // Write string to file "File.txt" using fprintf 
+   fputs (text, fp);                      // Write string to file "File.txt" using fputs
+
+   fputc ('\n', fp);                      // Added new line
+   fputc ('A', fp);                       // Added one character using fputc
+
+   fclose (fp);                           // Close a file "File.txt"
+
+} 
